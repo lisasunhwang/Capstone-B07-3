fa571515f68d23922dc09cb9d3f4546be42b0a03
lisasunhwang <lshwang@ucsd.edu>
2024-02-11 22:39:45 -0800

    move buildBST and getGRCIndex to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,3 @@
-from main import buildBST, getGRCIndex
-
 import gzip
 import json
 import pandas as pd
@@ -9,6 +7,38 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.colors as mcolors
+
+
+def buildBST(array,start=0,finish=-1):
+    if finish<0:
+        finish = len(array)
+    mid = (start + finish) // 2
+    if mid-start==1:
+        ltl=start
+    else:
+        ltl=buildBST(array,start,mid)
+    
+    if finish-mid==1:
+        gtl=mid
+    else:
+        gtl=buildBST(array,mid,finish)
+        
+    return((array[mid],ltl,gtl))
+
+def getGRCIndex(x,y,xbst,ybst):
+    while (type(xbst)==tuple):
+        if x < xbst[0]:
+            xbst=xbst[1]
+        else:
+            xbst=xbst[2]
+            
+    while (type(ybst)==tuple):
+        if y < ybst[0]:
+            ybst=ybst[1]
+        else:
+            ybst=ybst[2]
+            
+    return ybst, xbst
 
 
 pathname = 'NCSU-DigIC-GraphData-2023-07-25'
